6d1808df4010dbd78923519cb6f7fadb8ce7928b
Andrei2498 <48515205+Andrei2498@users.noreply.github.com>
2019-05-23 16:17:58 +0300

    asdas
    sadas

--- Client/src/view/ProfesorAbsente.java
+++ Client/src/view/ProfesorAbsente.java
@@ -1,4 +1,40 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
 public class ProfesorAbsente {
+
+    /*----Tabela de adaugare nota----*/
+    @FXML
+    TableView<AfisareNotaSimpla> tabelaAdaugareAbsente;
+    @FXML
+    TableColumn<AfisareNotaSimpla, Integer> coloanaID;
+    @FXML
+    TableColumn<AfisareNotaSimpla, String> coloanaNume;
+    @FXML
+    TableColumn<AfisareNotaSimpla,String> coloanaPrenume;
+    @FXML
+    private TextField filterFieldAdaugare;
+
+    /*----Tabela de vizionare nota----*/
+    @FXML
+    TableView<AfisareNote> tabelaVizionareAbsente;
+    @FXML
+    TableColumn<AfisareNote,String> coloanaVizionareNume;
+    @FXML
+    TableColumn<AfisareNote,String> coloanaVizionarePrenume;
+    @FXML
+    TableColumn<AfisareNote, LocalDate> coloanaVizionareData;
+    @FXML
+    private TextField filterFieldAfisare;
+
+
+
+
+
 }
