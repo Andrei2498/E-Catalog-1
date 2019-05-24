e22f556674ea85807ab287c292769860f0b2aed3
Marcel1123 <48123341+Marcel1123@users.noreply.github.com>
2019-05-24 11:58:44 +0300

    Set profil text

--- Client/src/view/Profesor.java
+++ Client/src/view/Profesor.java
@@ -3,10 +3,12 @@ package view;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,6 +22,8 @@ public class Profesor implements Initializable {
     private Text studentAge;
     @FXML
     private Text studentGender;
+    @FXML
+    private Text materieId;
 
     /*----Date despre institutia de invatamant si clasa pe care o frecventeaza studentul----*/
     @FXML
@@ -74,6 +78,41 @@ public class Profesor implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         firstName.setText(Client.profesor.getNume());
         secondName.setText(Client.profesor.getPrenume());
-        
+        studentAge.setText(Integer.toString(Client.profesor.getVarsta()));
+        studentGender.setText(Client.profesor.getGen());
+        titluLiceu.setText((Client.profesor.getLiceu().getTitlu()));
+        numeLiceu.setText(Client.profesor.getLiceu().getNume());
+        orasLiceu.setText(Client.profesor.getLiceu().getJudet());
+        anClasa.setText(Integer.toString(Client.profesor.getMaterie().getClasa()));
+        if(Client.profesor.getMaterie().getProfil().compareTo("real") == 0){
+            profilStudent.setLayoutX(profilStudent.getLayoutX()+10);
+            profilStudent.setText("Real");
+        } else {
+            profilStudent.setText("Uman");
+        }
+        studentJudet.setText(Client.profesor.getLiceu().getJudet());
+        studentLocalitate.setText(studentJudet.getText());
+        String[] adresa = Client.profesor.getAdresa().split(">>");
+        studentStrada.setText(adresa[0]);
+        if(adresa[1].compareTo("-") == 0){
+            studentBloc.setText("-");
+            studentScara.setText("-");
+            studentNumarLocuinta.setText(adresa[3]);
+        } else {
+            studentBloc.setText(adresa[1]);
+            studentScara.setText(adresa[2]);
+            studentNumarLocuinta.setText(adresa[3]);
+        }
+        studentNumarTelefon.setText(Client.profesor.getNr_telefon());
+        studentEmail.setText(Client.profesor.getEmail());
+        materieId.setText(Client.profesor.getMaterie().getNume());
+        File file;
+        if(studentGender.getText().compareTo("Masculin") == 0){
+            file=new File("src/img/male.png");
+        } else {
+            file=new File("src/img/female.png");
+        }
+        Image image=new Image(file.toURI().toString());
+        imagineProfil.setImage(image);
     }
 }
